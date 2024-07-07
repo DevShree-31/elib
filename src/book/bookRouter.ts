70a7dce8f0b95e4c359d206a8042eb9b7e150bef
Shreyansh <ui21cs56@iiitsurat.ac.in>
2024-07-07 14:14:22 +0530

    "Added updateBook function to bookController and corresponding route to bookRouter"

--- src/book/bookRouter.ts
+++ src/book/bookRouter.ts
@@ -1,5 +1,5 @@
 import express from "express";
-import { createBook } from "./bookController";
+import { createBook, updateBook } from "./bookController";
 import multer from "multer";
 import path from "node:path/win32";
 import authenticate from "../middlewares/authenticate";
@@ -14,5 +14,9 @@ bookRouter.post('/',authenticate,upload.fields([
     {name:'coverImage',maxCount:1},
     {name:'file',maxCount:1}
 ]),createBook);
+bookRouter.patch('/:bookId',authenticate,upload.fields([
+    {name:'coverImage',maxCount:1},
+    {name:'file',maxCount:1}
+]),updateBook);
 
 export default bookRouter
